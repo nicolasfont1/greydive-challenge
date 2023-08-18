85370f33c22383dcbc1b3d09ea2964c02aeaacb1
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-18 11:49:45 -0300

    reducer cleaned

--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -1,18 +1,7 @@
-import { REGISTER_USER, GET_USER, POST_ANSWERS, GET_ANSWERS, EDIT_ANSWERS} from "./action-types"
-
-const initialState = {
-  userId: ""
-}
+const initialState = {}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case REGISTER_USER:
-      let userId = action.payload
-
-      return {
-        ...state,
-        userId: userId
-      }
 
     default: return { ...state }
   }
